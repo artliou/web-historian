03db72c66297a871bce712c072a5452705bc2e30
Hack Reactor Student <student@hackreactor.com>
2017-07-26 16:46:11 -0700

    Implement addUrlToList

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -43,6 +43,10 @@ exports.isUrlInList = function(url, callback) {
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(exports.paths.list, url, (err)=> {
+    if (err) { throw err; }
+    callback();
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
