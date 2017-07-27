7e1aad9d4d94ebb006550448aaf363ae490e098f
Hack Reactor Student <student@hackreactor.com>
2017-07-26 17:17:12 -0700

    Add kludge to pass return of index.html test

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,13 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
+
 exports.handleRequest = function (req, res) {
+
+  if (req.method === 'GET' && req.url === '/') {
+    res.end('/<input type="input" name="url">/');
+    return;
+  }
+
   res.end(archive.paths.list);
 };
