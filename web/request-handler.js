2fa1909e27ce37e859ec8f55680dedfa4a3ca356
Hack Reactor Student <student@hackreactor.com>
2017-07-27 11:52:54 -0700

    Add 404 handling

--- web/request-handler.js
+++ web/request-handler.js
@@ -10,19 +10,33 @@ exports.handleRequest = function (req, res) {
       res.end('/<input type="input" name="url">/');
 
     } else {
+      var cb = function (isInArchive) {
+
+        if (isInArchive) {
+          res.statusCode = 200;
+          res.writeHead(res.statusCode, archive.headers);
+          archive.readArchivedFile(req.url, (data) => {
+            res.end(data);
+          });
+
+        } else {
+          // not present branch
+          res.statusCode = 404;
+          res.writeHead(res.statusCode, archive.headers);
+          res.end(); // we will need to return ./web/public/loading.html
+        }
+      };
+      archive.isUrlArchived( req.url, cb);
       // For all but '/':
-      res.statusCode = 200;
-      res.writeHead(res.statusCode, archive.headers);
-      archive.readArchivedFile(req.url, (data) => {
-        res.end(data);
-      });
     }
+
   } else if (req.method === 'POST') {
     var url = req.url.replace('/?', '');
-    archive.addUrlToList(url);
-    res.statusCode = 302;
-    res.writeHead(res.statusCode, archive.headers);
-    res.end();
+    archive.addUrlToList(url, () => {
+      res.statusCode = 302;
+      res.writeHead(res.statusCode, archive.headers);
+      res.end();
+    });
 
   } else {
     res.end(archive.paths.list);
